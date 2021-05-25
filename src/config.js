b69be363d9506926e05792103c98ea8ff13ddd1a
Ivan Dorohov <ivans.dorohovs@gmail.com>
2021-05-25 16:27:24 +0300

    config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 let config = {
-    baseUrl: 'http://127.0.0.1:5000', //MEMO: michael: local
+    baseUrl: 'https://polslv.micron.lv', //MEMO: michael: local
+    // baseUrl: 'http://127.0.0.1:5000', //MEMO: michael: prod
     accessTokenName: 'access_token',
     userRoles: {
         'guest': 0,
